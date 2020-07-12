b458dd71df3a72a49fb5dde8203546988b3133e2
Jhogne <hogne@student.chalmers.se>
2020-07-12 14:39:44 +0000

    Refactor Game and create GameState util

---
+++ frontend/src/screens/PromptText2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PromptText2 extends React.Component{
+    render() {
+        return(
+            <h1>Hello</h1>
+        );
+    }
+}
+
+export default PromptText2;
