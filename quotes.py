c8e57d5665e79b36e8eb2256ce89968b0d499828
Jhogne <hogne@student.chalmers.se>
2020-07-14 13:24:09 +0000

    Updated quotes script

--- quotes.py
+++ quotes.py
@@ -1,83 +1,120 @@
 import click
 from PyInquirer import prompt, print_json
+from wikiquote import DisambiguationPageException, NoSuchPageException
 import wikiquote
 import json
 
 jsonfile = "backend/src/main/data/data.json"
 
-results = wikiquote.search("surely you can't be serious")
-for r in results:
-    click.echo(r)
+@click.group()
+def cli():
+    """This script can be used to populate the databse with quotes from wikiquotes"""
+    pass
 
 
-@click.command()
+@cli.command()
 @click.argument("query")
-def quotes(query):
-    """Goes through the all wikiquotes from the given query."""
+def search(query):
+    """Search wikiquotes for a query."""
+
+    results = wikiquote.search(query)
+    click.echo(click.style("Found {} results:".format(len(results)), bold=True))
+    for result in results:
+        click.echo(result)
+
+
+@cli.command()
+@click.option(
+    '-n',
+    type=int, 
+    default=-1,
+    help="The number of quotes to be reviewed. Default is all.",
+)
+@click.option(
+    '-i',
+    type=int, 
+    default=-1,
+    help="The index of a specific quote to review.",
+)
+@click.argument("query")
+def quotes(query, n, i):
+    """Reviews quotes from wikiquotes based of the given query."""
 
     with open(jsonfile, 'r') as file:
         data = file.read()
 
-    obj = json.loads(data)
-    quotesList = wikiquote.quotes(query)
-    acceptedList = []
-    idx = 0
-    old_amt = len(obj)
+    all_quotes = json.loads(data)
+    try : 
+        quotes_list = wikiquote.quotes(query)
+    except DisambiguationPageException:
+        click.echo(click.style("ERROR: Disambiguation in prompt. Try searching first", fg="red"))
+        return
+    except NoSuchPageException:
+        click.echo(click.style("ERROR: No such page found. Try searching first", fg="red"))
+        return
+    if i > len(quotes_list):
+        click.echo(click.style("ERROR: Index out of bounds.", fg="red"))
+        return
+    if i > -1:
+        quotes_list = [quotes_list[i]]
 
-    while idx < len(quotesList):
+    idx = 0
+    old_amt = len(all_quotes)
+    end = len(quotes_list) if n < 0 or n > len(quotes_list) else n
+
+    while idx < end:
+        quote = format_quote(quotes_list[idx])
+        new_quote = {
+            "from" : query,
+            "quote" : quote
+        }
+
         click.clear()
         click.echo(click.style(query, fg='magenta', bold=True))
-        quote = format_quote(quotesList[idx])
-        res = prompt_quote(quote)
-        if res["answer"] == "Accept":
-            new_obj = {
-                "from" : query,
-                "quote" : quote
+        click.echo(click.style("{} out of {}".format(idx+1, end), bold=True), nl=False)
+        click.echo(click.style(" Already in" if new_quote in all_quotes else "", fg='green', bold=True))
+
+        click.echo(quote)
+        res = prompt([  
+            {
+                'type': 'rawlist',
+                'name': 'answer',
+                'choices': [
+                    'Accept',
+                    'Deny',
+                    'Edit',
+                    'Back',
+                    'Quit'
+                ],
+                'message': 'what to do?',
             }
-            if(new_obj in obj):
+        ])
+        if res["answer"] == "Accept":
+            if(new_quote in all_quotes):
                 click.echo("Quote alreay in database")
                 click.pause()
             else: 
-                obj.append(new_obj)
+                all_quotes.append(new_quote)
             idx += 1
         elif res["answer"] == "Edit":
             message = click.edit(f"{quote}")
-            quotesList.insert(idx+1, message)
+            quotes_list.insert(idx+1, message)
             idx += 1
         elif res["answer"] == "Back":
             idx -= 1
         elif res["answer"] == "Deny":
             idx += 1
         elif res["answer"] == "Quit":
-            idx = len(quotesList)
+            idx = end
 
     out_file = open(jsonfile, "w") 
-    json.dump(obj, out_file, indent = 6)
+    json.dump(all_quotes, out_file, indent = 6)
     out_file.close()
     click.clear()
-    click.echo("Added {} entries".format(len(obj) - old_amt))
-
+    click.echo("Added {} entries".format(len(all_quotes) - old_amt))
 
 def format_quote(quote):
-    return quote.replace('\n', '').encode('ascii', 'ignore').decode()
-
-def prompt_quote(quote):
-    click.echo(quote)
-    question =  [  
-    {
-        'type': 'rawlist',
-        'name': 'answer',
-        'choices': [
-            'Accept',
-            'Deny',
-            'Edit',
-            'Back',
-            'Quit'
-        ],
-        'message': 'what to do?',
-    }
-    ]
-    return prompt(question)
+    return quote.replace('\n', ' ').encode('ascii', 'ignore').decode()
 
 if __name__ == '__main__':
-    quotes()
+    cli()
