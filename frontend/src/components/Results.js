60f949db6aee4bdfb9a88c3c4b20e8cec3c14dc2
Jhogne <hogne@student.chalmers.se>
2020-08-07 15:34:52 +0000

    Icons in result

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import { Typography, Grid } from '@material-ui/core';
+import { Timer, ErrorOutline, Adjust } from '@material-ui/icons/';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => ({
+    left: {
+        justifyContent: "flex-end", 
+        marginRight: "20%", 
+        alignItems:"center",
+        display:"flex"
+    },
+  
+    right: {
+        marginLeft: "20%",
+        alignItems:"center",
+        display:"flex"
+    },
+  }));
 
 export default function Results(props) {
+    const classes = useStyles();
     return(
         <Grid 
             className={props.style}
@@ -9,25 +27,27 @@ export default function Results(props) {
             spacing={2}
             justify="center">
         <Grid item xs={6}>
-            <Typography align="right" style={{marginRight: '20%'}}>
-              Time: {Math.floor((props.time) / 1000)}s
+            <Typography className={classes.left} >
+              <Timer/>&nbsp;{Math.floor((props.time) / 1000)}s
             </Typography>
         </Grid>
         <Grid item xs={6}>
-        <Typography align="left" style={{marginLeft: '20%'}}>
-                Mistakes: {props.errors}
+
+        <Typography className={classes.right}>
+                <ErrorOutline/>&nbsp;{props.errors}
+            </Typography>
+
+
+        </Grid>
+        <Grid item xs={6}>
+            <Typography className={classes.left}>
+                # {props.words}
             </Typography>
 
         </Grid>
         <Grid item xs={6}>
-        <Typography align="right" style={{marginRight: '20%'}}>
-                Words: {props.words}
-            </Typography>
-
-        </Grid>
-        <Grid item xs={6}>
-            <Typography align="left" style={{marginLeft: '20%'}}>
-            Accuracy: {Math.round(props.accuracy * 10000)/100}%
+            <Typography className={classes.right}>
+                <Adjust/>&nbsp;{Math.round(props.accuracy * 10000)/100}%
             </Typography>
         </Grid>
         </Grid>
