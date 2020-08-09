669c69d61ad65b65f12ca20f85e1bd9bb3a4851f
Jhogne <hogne@student.chalmers.se>
2020-08-09 12:49:06 +0000

    Prettier

--- frontend/src/screens/Prompt.js
+++ frontend/src/screens/Prompt.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import Typography from '@material-ui/core/Typography'
+import Typography from "@material-ui/core/Typography";
 
 function divideText(text, current, error) {
-    return (
-      <>
-        <span className="correct">{text.slice(0, current)}</span>
-        <span className={error ? "error" : "remaining"}>
-          {text.slice(current, text.length)}
-        </span>
-      </>
-    );
-  }
+  return (
+    <>
+      <span className="correct">{text.slice(0, current)}</span>
+      <span className={error ? "error" : "remaining"}>
+        {text.slice(current, text.length)}
+      </span>
+    </>
+  );
+}
 
 export default function Prompt(props) {
-    return(
-        <div className="promptBox">
-          {!props.finished && <Typography variant="body1" className="default">{divideText(props.text, props.current, props.error)}</Typography>}
-        </div>
-    );
+  return (
+    <div className="promptBox">
+      {!props.finished && (
+        <Typography variant="body1" className="default">
+          {divideText(props.text, props.current, props.error)}
+        </Typography>
+      )}
+    </div>
+  );
 }
